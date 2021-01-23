5941b9a45de28bd06804f3107b30632b0b1deaf1
Brenda Ferrufino <ferrufinob@gmail.com>
2021-01-22 23:44:31 -0500

    Done.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require File.expand_path('../boot', __FILE__)
+require File.expand_path("../boot", __FILE__)
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,10 +22,10 @@ module OmniauthLab
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    
+
     # Commenting out this next line because it causes Chrome to cache the HTTPS protocol for the localhost
     # domain, forcing students to have to clear the cache to work
 
-    #config.force_ssl = true
+    # config.force_ssl = true
   end
 end
