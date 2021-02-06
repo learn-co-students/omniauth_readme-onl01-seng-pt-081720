ce55a944547b60c5e6c2044701d06dd24dc4e87c
tonitdiep <adiep123@gmail.com>
2021-02-05 21:04:41 -0500

    Done.

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,10 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  end
+
+
+
+# Rails syntax 
+# We're telling our Rails app to use a piece of
+# middleware created by OmniAuth for the Facebook authentication strategy.
+
